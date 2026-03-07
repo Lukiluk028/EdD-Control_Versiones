1cf26752d32be7d4b70044e81602c1a8f9f5ed78
CachopoDabuela <233050081+CachopoDabuela@users.noreply.github.com>
2026-03-07 20:07:51 +0100

    crear EmailService y extraer lógica de email a clase EmailService

---
+++ P4.1-notifications-base/EmailService.java
@@ -0,0 +1,6 @@
+public class EmailService implements NotificationService {
+    @Override
+    public void send(String message, String recipient) {
+        System.out.println("Enviando email a " + recipient + ": " + message);
+    }
+}
