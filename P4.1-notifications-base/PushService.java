082e89e68e73d66f347f6d4824d66a5fc32b041b
CachopoDabuela <233050081+CachopoDabuela@users.noreply.github.com>
2026-03-07 20:14:32 +0100

    crear PushService y extraer lógica de push a clase PushService

---
+++ P4.1-notifications-base/PushService.java
@@ -0,0 +1,6 @@
+public class PushService implements NotificationService {
+    @Override
+    public void send(String message, String recipient) {
+        System.out.println("Enviando push a " + recipient + ": " + message);
+    }
+}
