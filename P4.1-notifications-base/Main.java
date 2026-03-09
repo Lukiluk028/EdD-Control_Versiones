72fc46ffc4f214638c9a2a679cb9a05d37486e76
CachopoDabuela <233050081+CachopoDabuela@users.noreply.github.com>
2026-03-09 16:35:47 +0100

    Cambio de Main en base a los class añadidos

--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -3,10 +3,8 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager manager = new NotificationManager();
         
-        // Ejemplos de uso
-        manager.send("email", "Bienvenido al sistema", "usuario@email.com");
-        manager.send("sms", "Tu código es 1234", "+34123456789");
-        manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
-        
+        // Uso de la Factory para obtener el servicio
+        NotificationService email = NotificationFactory.getService("email");
+        manager.send(email, "Bienvenido al sistema", "usuario@email.com");
     }
 }
